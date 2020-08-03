e9ec0fa8ddde6c765e84db5e536ae4d94fa9959f
Eze Baravalle <ezebaravalle@gmail.com>
2020-08-03 00:44:13 -0300

    "Esta es una nueva Rama que Incorpora la Clase Common"

--- AplicacionDeConsolaBE/Program.cs
+++ AplicacionDeConsolaBE/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Security.AccessControl;
 using System.Text;
@@ -12,8 +13,21 @@ namespace AplicacionDeConsolaBE
     {
         static void Main(string[] args)
         {
+            Common var = new Common();
+            var.var1 = 23;
+            var.var2 = true;
+            int inicial = 22;
             System.Console.Write("Hola Mundo");
-            Thread.Sleep(2000);
+            
+           //Thread.Sleep(2000);
+            System.Console.WriteLine("Antes de la ejecución el valor de var1 y var dos es:"+ var.var1, var.var2);
+
+            var.setearEstado(inicial);
+
+            System.Console.WriteLine("Despues de la ejecución el valor de var1 y var dos es:" + var.var1, var.var2);
+
+            // Thread.Sleep(2000);
+            System.Console.Read();
         }
     }
 }
